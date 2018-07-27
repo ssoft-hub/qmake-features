23b2e3bca108917c598659eaa05e66c4ed340ef5
Stepanov.A.A <stepanov.a.a@topaz-atcs.com>
2018-07-27 09:43:49 +0300

    Update auto_version feature for windows.
    * Simplify auto version structs.
    * Append comments.
    * Test for windows.

--- include/AutoVersion.h
+++ include/AutoVersion.h
@@ -10,6 +10,9 @@ namespace AutoVersion
 {
     typedef const char * Chars;
 
+    /*!
+     * Safe wrapper (view) around 'const char *' string value.
+     */
     struct String
     {
         Chars c_str;
@@ -17,6 +20,13 @@ namespace AutoVersion
         String ( Chars chars = Chars() )
             : c_str( chars )
         {}
+
+        size_t length () const
+        {
+            return !c_str
+                ? size_t()
+                : strlen( c_str );
+        }
 
         String & operator = ( Chars chars )
         {
@@ -52,6 +62,12 @@ namespace AutoVersion
 
 namespace AutoVersion
 {
+    /*!
+     * Information about program module.
+     *
+     * This struct is typically handled by automatic procedure
+     * and rarely needs to be modified or handled by manual.
+     */
     struct Info
     {
         String product;
@@ -80,38 +96,50 @@ namespace AutoVersion
 
 namespace AutoVersion
 {
-    struct Dependency
-    {
-        Info build;
-        Info runtime;
-    };
-
+    struct Dependency;
     typedef ::std::list< Dependency > Dependencies;
-}
 
-namespace AutoVersion
-{
-    struct Component;
-    typedef ::std::list< Component > Components;
-
-    struct Component
+    /*!
+     * Full version information about program module.
+     * Version contains information about module and it's dependencies
+     * for module's build-time.
+     *
+     * This struct is typically handled by automatic procedure
+     * and rarely needs to be modified or handled by manual.
+     */
+    struct Version
     {
         Info info;
         Dependencies dependencies;
-        Components components;
     };
 
-    typedef ::std::list< Component > Components;
+    /*!
+     * Information about dependency of program module.
+     * Dependency contains information about submodule for parent module's
+     * build-time and full version information about submodule for submodule's
+     * build-time (parent module's run-time).
+     *
+     * This struct is typically handled by by automatic procedure
+     * and rarely needs to be modified or handled by manual.
+     */
+    struct Dependency
+    {
+        Info info;
+        Version version;
+    };
 }
 
 namespace AutoVersion
 {
-    extern Component component ();
+    extern Version version ();
 }
 
 namespace AutoVersion
 {
     // old C++ support
+    /*!
+     * Version status enumeration.
+     */
     struct Status
     {
         enum Enum
@@ -122,13 +150,30 @@ namespace AutoVersion
         };
     };
 
+    Status::Enum versionStatus ( const Dependency & value );
+    Status::Enum versionStatus ( const Version & value );
+
+    /*!
+     * Returns status of module dependency.
+     *
+     * If build-time revision not coincidences with run-time revision then
+     * method returns Status::Invalid. Else if submodule status is not valid
+     * (Status::Invalid or Status::Different) then method returns status of
+     * submodule. Else if build-time information fully coincidences with run-time
+     * information then method returns Status::Valid. In oposite case method
+     * returns Status::Different.
+     */
     inline Status::Enum versionStatus ( const Dependency & value )
     {
-        const Info & build = value.build;
-        const Info & runtime = value.runtime;
+        const Info & build = value.info;
+        const Info & runtime = value.version.info;
 
         if ( build.revision != runtime.revision )
             return Status::Invalid;
+
+        Status::Enum result = versionStatus( value.version );
+        if ( result != Status::Valid )
+            return result;
 
         if ( build.product == runtime.product
             && build.version == runtime.version
@@ -145,22 +190,20 @@ namespace AutoVersion
         return Status::Different;
     }
 
-    inline Status::Enum versionStatus ( const Component & value )
+    /*!
+     * Returns status of module version.
+     *
+     * Searches all status of module dependencies and returnds worst of them.
+     */
+    inline Status::Enum versionStatus ( const Version & value )
     {
         Status::Enum result = Status::Valid;
 
         // old C++ support
-        for ( Dependencies::const_iterator dep_iter = value.dependencies.cbegin();
-            result != Status::Invalid && dep_iter != value.dependencies.cend(); ++dep_iter )
+        for ( Dependencies::const_iterator iter = value.dependencies.cbegin();
+            result != Status::Invalid && iter != value.dependencies.cend(); ++iter )
         {
-            result = ::std::min( result, versionStatus( *dep_iter ) );
-        }
-
-        // old C++ support
-        for ( Components::const_iterator comp_iter = value.components.cbegin();
-            result != Status::Invalid && comp_iter != value.components.cend(); ++comp_iter )
-        {
-            result = ::std::min( result, versionStatus( *comp_iter ) );
+            result = ::std::min( result, versionStatus( *iter ) );
         }
 
         return result;
